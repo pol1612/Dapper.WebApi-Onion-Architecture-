ac4ecd50b1a400f3ec0d33319126e17af48deb23
Pol Sañé <psane@seidor.es>
2023-05-11 13:54:49 +0200

    Add project files.

---
+++ Dapper.Core/Device.cs
@@ -0,0 +1,16 @@
+﻿namespace Dapper.Core
+{
+    public class Device
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? TelemetryType { get; set; }
+
+        public Device(Guid id, string? name, string? telemetryType)
+        {
+            Id = id;
+            Name = name;
+            TelemetryType = telemetryType;
+        }
+    }
+}
