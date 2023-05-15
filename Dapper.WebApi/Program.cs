d0e8d6dd1d367fc114b0d79f99f291223281b0a5
Pol Sañé <psane@seidor.es>
2023-05-15 13:23:12 +0200

    added swagger

--- Dapper.WebApi/Program.cs
+++ Dapper.WebApi/Program.cs
@@ -1,8 +1,18 @@
+using Dapper.WebApi.Extensions;
+using Swashbuckle.AspNetCore;
+using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-
+builder.Services.AddControllers();
+builder.Services.AddInfrastructure();
+builder.Services.AddSwaggerGen
+    (
+    c => c.SwaggerDoc("V1", new OpenApiInfo { Title = "Dapper.WebApi", Version = "V1"})
+    );
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -12,6 +22,11 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{id?}");
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
