2ce3b05db63b3cb280969d5cd7ce844f654214c9
Pol Sañé <psane@seidor.es>
2023-05-18 09:09:04 +0200

    added a class mapper for the devices table and the device entity

--- Dapper.Core/Entities/Device.cs
+++ Dapper.Core/Entities/Device.cs
@@ -2,11 +2,11 @@
 {
     public class Device
     {
-        public Guid Id { get; set; }
+        public Guid? Id { get; set; }
         public string? Name { get; set; }
         public string? TelemetryType { get; set; }
 
-        public Device(string name, string telemetryType, Guid? id)
+        public Device(string name, string telemetryType, Guid? id = null)
         {
             Id = id ?? Guid.NewGuid();
             Name = name;
---
+++ Dapper.Infrastructure/Mappers/DeviceMapper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper.Core.Entities;
+using DapperExtensions.Mapper;
+
+namespace Dapper.Infrastructure.Mappers
+{
+    public class DeviceMapper : AutoClassMapper<Device>
+    {
+        public DeviceMapper()
+        {
+            Table("Devices");
+            Console.WriteLine("it maps");
+            AutoMap();
+        }
+    }
+}
